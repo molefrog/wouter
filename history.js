80a0cfe34e7e92679f0ad27b6eedf617ff4f4dbc
Alexey Taktarov <molefrog@gmail.com>
2019-04-11 00:22:52 +0300

    Implement history.js and specs.

--- history.js
+++ history.js
@@ -1,2 +1,49 @@
-import stub from "./test/memory-history";
-export default stub;
+export default function createHistory() {
+  monkeyPatchHistoryEvents();
+
+  return {
+    path: () => window.location.pathname,
+    push: to => window.history.replaceState({}, null, to),
+    subscribe: cb => {
+      const handler = () => cb(window.location.pathname);
+      return on(handler, "popState", "pushState", "replaceState");
+    }
+  };
+}
+
+// subscribe to multiple window events at once
+const on = (cb, ...events) => {
+  events.map(e => window.addEventListener(e, cb));
+  return () => events.map(e => window.removeEventListener(e, cb));
+};
+
+// While History API does have `popState` event, the only
+// proper way to listen to changes via `push/replaceState`
+// is to monkey-patch these methods.
+//
+// See https://stackoverflow.com/a/4585031
+const monkeyPatchHistoryEvents = (() => {
+  let patched = false;
+
+  return () => {
+    if (patched) return false;
+
+    const patchEvent = type => {
+      const orig = history[type];
+
+      return function() {
+        const result = orig.apply(this, arguments);
+        const event = new Event(type);
+        event.arguments = arguments;
+        window.dispatchEvent(event);
+
+        return result;
+      };
+    };
+
+    history.pushState = patchEvent("pushState");
+    history.replaceState = patchEvent("replaceState");
+
+    return (patched = true);
+  };
+})();
