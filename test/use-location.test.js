d5b20ba64c53ce74adbd065b72d3371c427dadd3
Alexey Taktarov <molefrog@gmail.com>
2019-05-13 17:49:12 +0200

    Add specs for `useLocation` hook.

---
+++ test/use-location.test.js
@@ -0,0 +1,68 @@
+import { renderHook, act } from "react-hooks-testing-library";
+import { useLocation } from "../index.js";
+
+it("returns a pair [value, update]", () => {
+  const { result, unmount } = renderHook(() => useLocation());
+  const [value, update] = result.current;
+
+  expect(typeof value).toBe("string");
+  expect(typeof update).toBe("function");
+  unmount();
+});
+
+describe("`value` first argument", () => {
+  beforeEach(() => history.replaceState(0, 0, "/"));
+
+  it("reflects the current pathname", () => {
+    const { result, unmount } = renderHook(() => useLocation());
+    expect(result.current[0]).toBe("/");
+    unmount();
+  });
+
+  it("reacts to `pushState` / `replaceState`", () => {
+    const { result, unmount } = renderHook(() => useLocation());
+
+    act(() => history.pushState(0, 0, "/foo"));
+    expect(result.current[0]).toBe("/foo");
+
+    act(() => history.replaceState(0, 0, "/bar"));
+    expect(result.current[0]).toBe("/bar");
+    unmount();
+  });
+
+  it("supports history.back() navigation", () => {
+    jest.useFakeTimers();
+    const { result, unmount } = renderHook(() => useLocation());
+
+    act(() => history.pushState(0, 0, "/foo"));
+    expect(result.current[0]).toBe("/foo");
+
+    act(() => {
+      history.back();
+      jest.runAllTimers();
+    });
+
+    expect(result.current[0]).toBe("/");
+    unmount();
+  });
+});
+
+describe("`update` second parameter", () => {
+  it("rerenders the component", () => {
+    const { result, unmount } = renderHook(() => useLocation());
+    const update = result.current[1];
+
+    act(() => update("/about"));
+    expect(result.current[0]).toBe("/about");
+    unmount();
+  });
+
+  it("changes the current location", () => {
+    const { result, unmount } = renderHook(() => useLocation());
+    const update = result.current[1];
+
+    act(() => update("/about"));
+    expect(location.pathname).toBe("/about");
+    unmount();
+  });
+});
