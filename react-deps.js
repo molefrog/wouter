8dad312a115314864808560d8302516bcb21be73
Alexander Tolkunov <tolkunov@kinoplan.ru>
2019-05-19 23:02:12 +0300

    Create a project structure for Preact

---
+++ react-deps.js
@@ -0,0 +1,26 @@
+import {
+  useRef,
+  useMemo,
+  useEffect,
+  useState,
+  useContext,
+  useCallback,
+  createContext,
+  isValidElement,
+  cloneElement,
+  createElement
+} from "react";
+
+export {
+  useRef,
+  useMemo,
+  useEffect,
+  useState,
+  useContext,
+  useCallback,
+  createContext,
+  isValidElement,
+  cloneElement,
+  createElement
+}
+
