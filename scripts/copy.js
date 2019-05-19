f26826ae2ce59fe13d29acb55e891c54ca178cb0
Alexander Tolkunov <tolkunov@kinoplan.ru>
2019-05-19 23:08:45 +0300

    Add a prepublish script

---
+++ scripts/copy.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+var copyfiles = require('copyfiles');
+
+copyfiles(
+    ["./index.js", "./history.js", "./matcher.js", "./extra/**", "./preact"],
+    {
+      verbose: true,
+      error: true
+    },
+    function(error) {
+      if (error) {
+        console.log("\x1b[41m%s\x1b[0m", "ERROR!", error.message);
+      } else {
+        console.log("\x1b[32m%s\x1b[0m", "Copied successfully!");
+      }
+    }
+);
