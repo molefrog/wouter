2674298f0443fe87251eaf6ecbc42ffb80ab2ace
Alexander Tolkunov <tolkunov@kinoplan.ru>
2019-05-20 20:30:20 +0300

    Fix exports

--- preact/react-deps.js
+++ preact/react-deps.js
@@ -1,27 +1,10 @@
-import {
-  createContext,
-  cloneElement,
-  createElement
-} from "preact";
-import {
-  useRef,
-  useMemo,
-  useEffect,
-  useState,
-  useContext,
-  useCallback
-} from 'preact/hooks';
-import {isValidElement} from 'preact/compat';
-  
+export { createContext, cloneElement, createElement } from "preact";
+export { isValidElement } from "preact/compat";
 export {
   useRef,
   useMemo,
   useEffect,
   useState,
   useContext,
-  useCallback,
-  createContext,
-  cloneElement,
-  createElement,
-  isValidElement
-}
+  useCallback
+} from "preact/hooks";
