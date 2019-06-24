96dc166bf008e28f96802c4afaedbf3cb9e2eb3e
jeetiss <jeetiss@yandex.ru>
2019-06-24 20:58:15 +0300

    add useEffect deps

--- use-location.js
+++ use-location.js
@@ -11,7 +11,7 @@ export default () => {
 
     events.map(e => addEventListener(e, handler));
     return () => events.map(e => removeEventListener(e, handler));
-  });
+  }, []);
 
   return [path, to => history.pushState(0, 0, to)];
 };
