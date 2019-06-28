0ab5f3ae6d0acfa3c14880d2d813456704e61af5
Alexey Taktarov <molefrog@gmail.com>
2019-06-28 18:19:12 +0300

    Remove unused `useMemo` hook from react-deps.

--- preact/react-deps.js
+++ preact/react-deps.js
@@ -2,7 +2,6 @@ export { createContext, cloneElement, createElement } from "preact";
 export { isValidElement } from "preact/compat";
 export {
   useRef,
-  useMemo,
   useEffect,
   useState,
   useContext,
