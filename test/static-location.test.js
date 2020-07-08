1dc47696fb4408451b6942749d23cc398472421d
David Evans <davidje13@users.noreply.github.com>
2020-07-08 20:28:42 +0100

    Add static location hook history [#113]

--- test/static-location.test.js
+++ test/static-location.test.js
@@ -24,3 +24,41 @@ it("doesn't change the value even if updated", () => {
 
   expect(result.current[0]).toBe("/try-changing-me");
 });
+
+it("records no history by default", () => {
+  const hook = staticLocation("/page1");
+  const { result } = renderHook(() => hook());
+  const [, update] = result.current;
+
+  act(() => {
+    update("/page2");
+  });
+
+  expect(hook.history).toEqual(["/page1"]);
+});
+
+it("records history if requested but does not change the value", () => {
+  const hook = staticLocation("/page1", { record: true });
+  const { result } = renderHook(() => hook());
+  const [, update] = result.current;
+
+  act(() => {
+    update("/page2");
+  });
+
+  expect(hook.history).toEqual(["/page1", "/page2"]);
+  expect(result.current[0]).toBe("/page1");
+});
+
+it("respects the 'replace' option", () => {
+  const hook = staticLocation("/page1", { record: true });
+  const { result } = renderHook(() => hook());
+  const [, update] = result.current;
+
+  act(() => {
+    update("/page2", { replace: true });
+  });
+
+  expect(hook.history).toEqual(["/page2"]);
+  expect(result.current[0]).toBe("/page1");
+});
