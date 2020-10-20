c499c4ec0f1c316207b0b12958b5d715f4ef6279
huangteng02 <huangteng02@meituan.com>
2020-10-20 22:39:31 +0800

    feat: add unit test

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,3 +1,13 @@
 import { useState } from "react";
 
 export const memoryLocation = (path = "/") => () => useState(path);
+
+export const customHook = (initialPath = "/") => () => {
+  const [path, updatePath] = useState(initialPath);
+  const navigate = path => {
+    updatePath(path);
+    return 'foo';
+  }
+
+  return [path, navigate];
+}
