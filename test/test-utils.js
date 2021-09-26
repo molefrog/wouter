1dc2c84dd3a06345c6230554900ca4df89870df2
Sebastien Lacoste <contact@graphman.fr>
2021-09-26 20:32:48 +0200

    Make infered params backward compatible and work with useRoute

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
-export const memoryLocation = (path = "/") => () => useState(path);
+export const memoryLocation =
+  (path = "/") =>
+  () =>
+    useState(path);
 
-export const customHookWithReturn = (initialPath = "/") => () => {
-  const [path, updatePath] = useState(initialPath);
-  const navigate = (path) => {
-    updatePath(path);
-    return "foo";
+export const customHookWithReturn =
+  (initialPath = "/") =>
+  () => {
+    const [path, updatePath] = useState(initialPath);
+    const navigate = (path) => {
+      updatePath(path);
+      return "foo";
+    };
+
+    return [path, navigate];
   };
-
-  return [path, navigate];
-};
