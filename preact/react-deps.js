7f89aaabf215cb740627a96c235780fa42e2cdf8
Hans Brende <hansbrende@apache.org>
2022-11-18 11:39:23 -0600

    copy latest preact implementation of uSES & revert to dependency on 10.0.0

--- preact/react-deps.js
+++ preact/react-deps.js
@@ -1,3 +1,4 @@
+import { useState, useLayoutEffect, useEffect } from "preact/hooks";
 export {
   isValidElement,
   createContext,
@@ -14,7 +15,40 @@ export {
   useCallback,
 } from "preact/hooks";
 
-export { useSyncExternalStore } from "preact/compat";
+// TODO: switch to "export { useSyncExternalStore } from "preact/compat" once we update Preact to >= 10.11.3
+function is(x, y) {
+  return (x === y && (x !== 0 || 1 / x === 1 / y)) || (x !== x && y !== y);
+}
+export function useSyncExternalStore(subscribe, getSnapshot) {
+  const value = getSnapshot();
+
+  const [{ _instance }, forceUpdate] = useState({
+    _instance: { _value: value, _getSnapshot: getSnapshot }
+  });
+
+  useLayoutEffect(() => {
+    _instance._value = value;
+    _instance._getSnapshot = getSnapshot;
+
+    if (!is(_instance._value, getSnapshot())) {
+      forceUpdate({ _instance });
+    }
+  }, [subscribe, value, getSnapshot]);
+
+  useEffect(() => {
+    if (!is(_instance._value, _instance._getSnapshot())) {
+      forceUpdate({ _instance });
+    }
+
+    return subscribe(() => {
+      if (!is(_instance._value, _instance._getSnapshot())) {
+        forceUpdate({ _instance });
+      }
+    });
+  }, [subscribe]);
+
+  return value;
+}
 
 // provide forwardRef stub for preact
 export function forwardRef(component) {
