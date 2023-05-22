e2e73cb94712444c8ebe7d9e5706ebaacc79c5cb
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-22 15:06:56 +0300

    move path functions to separate file

--- use-location.js
+++ use-location.js
@@ -1,16 +1,5 @@
 import { useSyncExternalStore, useEvent } from "./react-deps.js";
-
-/*
- * Transforms `path` into its relative `base` version
- * If base isn't part of the path provided returns absolute path e.g. `~/app`
- */
-const relativePath = (base = "", path = location.pathname) =>
-  !path.toLowerCase().indexOf(base.toLowerCase())
-    ? path.slice(base.length) || "/"
-    : "~" + path;
-
-const absolutePath = (to, base = "") =>
-  to[0] === "~" ? to.slice(1) : base + to;
+import { absolutePath, relativePath } from "./path-functions.js";
 
 /**
  * History API docs @see https://developer.mozilla.org/en-US/docs/Web/API/History
