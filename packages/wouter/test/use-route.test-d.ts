f833ef0686f917f80fbcf5fb5c63da587a923124
Alexey Taktarov <molefrog@gmail.com>
2023-09-13 12:27:24 +0200

    Type specs for useRoute.

---
+++ packages/wouter/test/use-route.test-d.ts
@@ -0,0 +1,49 @@
+import { it, expectTypeOf, assertType, assert, expect } from "vitest";
+import { useRoute } from "wouter";
+
+it("should only accept strings", () => {
+  // @ts-expect-error
+  assertType(useRoute(Symbol()));
+  // @ts-expect-error
+  assertType(useRoute());
+  assertType(useRoute("/"));
+});
+
+it('has a boolean "match" result as a first returned value', () => {
+  const [match] = useRoute("/");
+  expectTypeOf(match).toEqualTypeOf<boolean>();
+});
+
+it("returns null as parameters when there was no match", () => {
+  const [match, params] = useRoute("/foo");
+
+  if (!match) {
+    expectTypeOf(params).toEqualTypeOf<null>();
+  }
+});
+
+it("accepts the type of parameters as a generic argument", () => {
+  const [match, params] = useRoute<{ id: string; name: string | undefined }>(
+    "/app/users/:name?/:id"
+  );
+
+  if (match) {
+    expectTypeOf(params).toEqualTypeOf<{
+      id: string;
+      name: string | undefined;
+    }>();
+  }
+});
+
+it("infers parameters from the route path", () => {
+  const [, inferedParams] = useRoute("/app/users/:name?/:id");
+
+  if (inferedParams) {
+    expectTypeOf(inferedParams).toMatchTypeOf<{
+      name: string | undefined;
+      id: string;
+    }>();
+  }
+});
+
+it.todo("accepts custom parser type for parameter inference");
