21ff758f2eb3528b9284d27fb24fdd71d3772af0
Mikael Lindqvist <li.mikael@gmail.com>
2023-09-24 17:59:47 +0800

    don't use global location, because it doesn't work on cloudflare workers

--- paths.js
+++ paths.js
@@ -3,13 +3,12 @@
  * If base isn't part of the path provided returns absolute path e.g. `~/app`
  */
 export const relativePath = (base = "", path = "") => {
-  if (!path && typeof location!="undefined")
-      path=location.pathname;
+  if (!path && typeof location != "undefined") path = location.pathname;
 
   return !path.toLowerCase().indexOf(base.toLowerCase())
     ? path.slice(base.length) || "/"
     : "~" + path;
-}
+};
 
 export const absolutePath = (to, base = "") =>
   to[0] === "~" ? to.slice(1) : base + to;
--- use-location.js
+++ use-location.js
@@ -30,26 +30,23 @@ export const useLocationProperty = (fn, ssrFn) =>
   useSyncExternalStore(subscribeToLocationUpdates, fn, ssrFn);
 
 const currentSearch = () => {
-  if (typeof location!="undefined")
-    location.search;
-}
+  if (typeof location != "undefined") location.search;
+};
 
 export const useSearch = () => useLocationProperty(currentSearch);
 
 const currentPathname = () => {
-  if (typeof location!="undefined")
-    return location.pathname;
-}
+  if (typeof location != "undefined") return location.pathname;
+};
 
 export const usePathname = ({ ssrPath } = {}) => {
-  if (ssrPath)
-    return ssrPath;
+  if (ssrPath) return ssrPath;
 
   return useLocationProperty(
     currentPathname,
     ssrPath ? () => ssrPath : currentPathname
   );
-}
+};
 
 export const navigate = (to, { replace = false } = {}) =>
   history[replace ? eventReplaceState : eventPushState](null, "", to);
