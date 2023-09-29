d86f4dc1c69ecab2fe4c10488196d04774604bfc
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-09-29 14:50:42 +0300

    add one more type test

--- packages/wouter/test/use-browser-location.test-d.ts
+++ packages/wouter/test/use-browser-location.test-d.ts
@@ -8,7 +8,23 @@ it("should return string, function tuple", () => {
   assertType<Function>(navigate);
 });
 
-it("should support options", () => {
+it("should return `navigate` function with `path` and `options` parameters", () => {
+  const [, navigate] = useBrowserLocation();
+
+  assertType(navigate("/path"));
+  assertType(navigate(""));
+
+  // @ts-expect-error
+  assertType(navigate());
+  // @ts-expect-error
+  assertType(navigate(null));
+
+  assertType(navigate("/path", { replace: true }));
+  // @ts-expect-error
+  assertType(navigate("/path", { unknownOption: true }));
+});
+
+it("should support base option", () => {
   assertType(useBrowserLocation({ base: "/something" }));
   // @ts-expect-error
   assertType(useBrowserLocation({ foo: "bar" }));
