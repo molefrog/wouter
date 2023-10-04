44ad6ee9c5ba911bc01f0b8b8fd51bfd19e73906
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-10-04 13:51:16 +0300

    add type checks for `useSearch` and `useHistoryState`

--- packages/wouter/test/use-browser-location.test-d.ts
+++ packages/wouter/test/use-browser-location.test-d.ts
@@ -1,31 +1,60 @@
-import { it, assertType } from "vitest";
-import { useBrowserLocation } from "wouter/use-browser-location";
+import { it, assertType, describe, expectTypeOf } from "vitest";
+import {
+  useBrowserLocation,
+  useSearch,
+  useHistoryState,
+} from "wouter/use-browser-location";
 
-it("should return string, function tuple", () => {
-  const [loc, navigate] = useBrowserLocation();
+describe("useBrowserLocation", () => {
+  it("should return string, function tuple", () => {
+    const [loc, navigate] = useBrowserLocation();
 
-  assertType<string>(loc);
-  assertType<Function>(navigate);
+    assertType<string>(loc);
+    assertType<Function>(navigate);
+  });
+
+  it("should return `navigate` function with `path` and `options` parameters", () => {
+    const [, navigate] = useBrowserLocation();
+
+    assertType(navigate("/path"));
+    assertType(navigate(""));
+
+    // @ts-expect-error
+    assertType(navigate());
+    // @ts-expect-error
+    assertType(navigate(null));
+
+    assertType(navigate("/path", { replace: true }));
+    // @ts-expect-error
+    assertType(navigate("/path", { unknownOption: true }));
+  });
+
+  it("should support base option", () => {
+    assertType(useBrowserLocation({ base: "/something" }));
+    // @ts-expect-error
+    assertType(useBrowserLocation({ foo: "bar" }));
+  });
 });
 
-it("should return `navigate` function with `path` and `options` parameters", () => {
-  const [, navigate] = useBrowserLocation();
+describe("useSearch", () => {
+  it("should return string", () => {
+    const search = useSearch();
 
-  assertType(navigate("/path"));
-  assertType(navigate(""));
-
-  // @ts-expect-error
-  assertType(navigate());
-  // @ts-expect-error
-  assertType(navigate(null));
-
-  assertType(navigate("/path", { replace: true }));
-  // @ts-expect-error
-  assertType(navigate("/path", { unknownOption: true }));
+    assertType<string>(search);
+  });
 });
 
-it("should support base option", () => {
-  assertType(useBrowserLocation({ base: "/something" }));
-  // @ts-expect-error
-  assertType(useBrowserLocation({ foo: "bar" }));
+describe("useHistoryState", () => {
+  it("should support generics", () => {
+    type TestCase = { hello: string };
+    const state = useHistoryState<TestCase>();
+
+    expectTypeOf(state).toEqualTypeOf<TestCase>();
+  });
+
+  it("should fallback to any when type doesn't provided", () => {
+    const state = useHistoryState();
+
+    expectTypeOf(state).toEqualTypeOf<any>();
+  });
 });
