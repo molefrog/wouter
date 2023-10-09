dcab932c446290278299a0d745d9e28e13c1d75b
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-10-09 15:49:26 +0300

    add memory location spec

---
+++ packages/wouter/test/memory-location.test-d.ts
@@ -0,0 +1,44 @@
+import { it, assertType, expectTypeOf } from "vitest";
+// @ts-expect-error todo: implement memory location
+import { memoryLocation } from "wouter/memory-location";
+
+it("should return hook that supports location spec", () => {
+  const { hook } = memoryLocation();
+
+  assertType<Function>(hook);
+
+  const [location, navigate] = hook();
+
+  assertType<string>(location);
+  assertType<Function>(navigate);
+});
+
+it("should return `navigate` method for navigating outside of components", () => {
+  const { navigate } = memoryLocation();
+
+  assertType<Function>(navigate);
+});
+
+it("should support `record` option for saving the navigation history", () => {
+  const { history } = memoryLocation({ record: true });
+
+  assertType<string[]>(history);
+});
+
+it("should support initial path", () => {
+  const { hook } = memoryLocation({ path: "/initial-path" });
+
+  assertType<Function>(hook);
+});
+
+it("should support `static` option", () => {
+  const { hook } = memoryLocation({ static: true });
+
+  assertType<Function>(hook);
+});
+
+it("should support `static` option", () => {
+  const { hook } = memoryLocation({ static: true });
+
+  assertType<Function>(hook);
+});
---
+++ packages/wouter/test/memory-location.test.ts
@@ -0,0 +1,57 @@
+import { it, expect } from "vitest";
+import { renderHook, act, waitFor } from "@testing-library/react";
+// @ts-expect-error todo: implement memory location
+import { memoryLocation } from "wouter/memory-location";
+
+it("returns a hook that is compatible with location spec", () => {
+  const { hook } = memoryLocation();
+
+  const { result, unmount } = renderHook(() => hook());
+  const [value, update] = result.current;
+
+  expect(typeof value).toBe("string");
+  expect(typeof update).toBe("function");
+  unmount();
+});
+
+it("should support initial path", () => {
+  const { hook } = memoryLocation({ path: "/test-case" });
+
+  const { result, unmount } = renderHook(() => hook());
+  const [value] = result.current;
+
+  expect(value).toBe("/test-case");
+  unmount();
+});
+
+it('should return location hook that has initial path "/" by default', () => {
+  const { hook } = memoryLocation();
+
+  const { result, unmount } = renderHook(() => hook());
+  const [value] = result.current;
+
+  expect(value).toBe("/");
+  unmount();
+});
+
+it("should return location hook that supports `base` option for nested routing", () => {
+  const { hook } = memoryLocation();
+
+  const { result, unmount } = renderHook(() => hook({ base: "/nested" }));
+  const [value] = result.current;
+
+  expect(value).toBe("/nested");
+  unmount();
+});
+
+it("should return standalone `navigate` method", () => {
+  const { hook, navigate } = memoryLocation();
+
+  const { result, unmount } = renderHook(() => hook());
+
+  act(() => navigate("/standalone"));
+
+  const [value] = result.current;
+  expect(value).toBe("/standalone");
+  unmount();
+});
