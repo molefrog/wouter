f29998bbc129672e7455b9c03fd41d07326225dd
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-10-12 14:28:46 +0300

    add tests for link's ref

--- packages/wouter/test/link.test-d.tsx
+++ packages/wouter/test/link.test-d.tsx
@@ -1,5 +1,6 @@
 import { describe, it, assertType } from "vitest";
 import { Link } from "wouter";
+import * as React from "react";
 
 describe("Link types", () => {
   it("should have required prop href", () => {
@@ -30,4 +31,44 @@ describe("Link types", () => {
       </Link>
     );
   });
+});
+
+describe("Link's ref", () => {
+  it("should work", () => {
+    const ref = React.useRef<HTMLAnchorElement>(null);
+
+    <Link to="/" ref={ref}>
+      HELLO
+    </Link>;
+  });
+
+  it("should have error when type is `unknown`", () => {
+    const ref = React.useRef();
+
+    // @ts-expect-error
+    <Link to="/" ref={ref}>
+      HELLO
+    </Link>;
+  });
+
+  it("should have error when type is miss matched", () => {
+    const ref = React.useRef<HTMLAreaElement>(null);
+
+    // @ts-expect-error
+    <Link to="/" ref={ref}>
+      HELLO
+    </Link>;
+  });
+
+  it("should work with composed components", () => {
+    const ref = React.useRef<React.ElementRef<typeof Component>>(null);
+
+    <Link to="/">
+      <Component ref={ref}></Component>
+    </Link>;
+  });
+});
+
+const Component = React.forwardRef<{ hello: "world" }>((props, ref) => {
+  return <>test</>;
 });
