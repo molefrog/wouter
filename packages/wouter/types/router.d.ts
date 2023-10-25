70c04cfe63f82a26ae5ca5def713bf251f084ca4
Alexey Taktarov <molefrog@gmail.com>
2023-10-25 14:48:07 +0200

    Types for React.

--- packages/wouter/types/router.d.ts
+++ packages/wouter/types/router.d.ts
@@ -1,20 +1,29 @@
-import { Path, BaseLocationHook } from "./location-hook";
+import {
+  Path,
+  SearchString,
+  BaseLocationHook,
+  BaseSearchHook,
+} from "./location-hook";
 
 export type Parser = (route: Path) => { pattern: RegExp; keys: string[] };
 
 // the object returned from `useRouter`
 export interface RouterObject {
   readonly hook: BaseLocationHook;
+  readonly searchHook: BaseSearchHook;
   readonly base: Path;
   readonly ownBase: Path;
   readonly parser: Parser;
   readonly ssrPath?: Path;
+  readonly ssrSearch?: SearchString;
 }
 
 // basic options to construct a router
 export type RouterOptions = {
   hook?: BaseLocationHook;
+  searchHook?: BaseSearchHook;
   base?: Path;
   parser?: Parser;
   ssrPath?: Path;
+  ssrSearch?: SearchString;
 };
