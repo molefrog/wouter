f71aecd14e1558784e9f7b876f6ea05f6c53679f
Alexey Taktarov <molefrog@gmail.com>
2023-11-01 15:31:15 +0100

    Ship `regexparam` with optional wildcard support.

---
+++ packages/wouter-preact/types/regexparam.d.ts
@@ -0,0 +1,34 @@
+export function parse(
+  route: string,
+  loose?: boolean
+): {
+  keys: string[];
+  pattern: RegExp;
+};
+
+export function parse(route: RegExp): {
+  keys: false;
+  pattern: RegExp;
+};
+
+export type RouteParams<T extends string> =
+  T extends `${infer Prev}/*/${infer Rest}`
+    ? RouteParams<Prev> & { wild: string } & RouteParams<Rest>
+    : T extends `${string}:${infer P}?/${infer Rest}`
+    ? { [K in P]?: string } & RouteParams<Rest>
+    : T extends `${string}:${infer P}/${infer Rest}`
+    ? { [K in P]: string } & RouteParams<Rest>
+    : T extends `${string}:${infer P}?`
+    ? { [K in P]?: string }
+    : T extends `${string}:${infer P}`
+    ? { [K in P]: string }
+    : T extends `${string}*`
+    ? { wild: string }
+    : T extends `${string}*?`
+    ? { wild?: string }
+    : {};
+
+export function inject<T extends string>(
+  route: T,
+  values: RouteParams<T>
+): string;
