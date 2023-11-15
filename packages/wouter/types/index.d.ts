28c7bd4a6e040dc27dd80ab4555f04e0a2bcc18a
Alexey Taktarov <molefrog@gmail.com>
2023-11-15 09:59:50 +0100

    Remove `PropsWithChildren` from Redirect type definition.

--- packages/wouter/types/index.d.ts
+++ packages/wouter/types/index.d.ts
@@ -91,7 +91,7 @@ export type RedirectProps<H extends BaseLocationHook = BrowserLocationHook> =
   };
 
 export function Redirect<H extends BaseLocationHook = BrowserLocationHook>(
-  props: PropsWithChildren<RedirectProps<H>>,
+  props: RedirectProps<H>,
   context?: any
 ): null;
 
